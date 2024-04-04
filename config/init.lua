83a5e914a954db08bca511c5fc8d20931f9755ff
Bravegrumpy <code@bravegrumpy.com>
2024-04-04 18:55:02 -0400

    Adding editconfig unique command

--- config/init.lua
+++ config/init.lua
@@ -3,6 +3,30 @@
 -- I had before adding/dealing with external plugins
 
 vim.o.ignorecase = true
-vim.o.hls = false
+vim.api.nvim_command('set nohlsearch')
+vim.highlight.on_yank()
 vim.o.number = true
 vim.o.relativenumber = true
+vim.o.showcmd = true
+vim.o.shiftwidth = 4
+vim.o.smarttab = true
+
+
+
+-- Using API to create "Initialize Config" Command
+vim.api.nvim_create_user_command(
+    "InitalizeConfig",
+    function()
+	vm.fn.mkdir(vim.fn.stdpath("config"), "p")
+    end,
+    {}
+)
+
+-- Using API to create "EditConfig" command
+vim.api.nvim_create_user_command(
+    "EditConfig",
+    function()
+	vim.cmd("edit " .. vim.fn.stdpath("config") .. "/init.lua")
+    end,
+    {}
+)
